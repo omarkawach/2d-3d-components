f1cffb3af26fd00b93499a3a8937210b89b33d6c
Omar Kawach <OKawach@esri.com>
2024-04-03 13:55:28 -0700

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "https://omarkawach.github.io/2d-3d-components/"
+      ? "/2d-3d-components/"
       : "/",
 };
