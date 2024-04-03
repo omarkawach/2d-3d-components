2012332f73483e128214bde6d293074bf860eaae
Omar Kawach <OKawach@esri.com>
2024-04-03 13:45:33 -0700

    gh pages

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "https://omarkawach.github.io/2d-3d-components/" // replace 'your-repo' with the name of your GitHub repository
+      : "/",
+};
