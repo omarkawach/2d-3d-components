98604e055f5c86b2a22231a62a5ad1e1b248f09e
Omar Kawach <OKawach@esri.com>
2024-04-03 14:01:10 -0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/2d-3d-components/',
   plugins: [
     vue({
       template: {
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/2d-3d-components/"
-      : "/",
-};
